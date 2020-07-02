44e86bc4db1b32329261ad685a5b8d1de70ee3cf
Daniel Rojas <daniel.drojas@outlook.com>
2020-07-02 01:23:18 -0400

    got user's input and stored in array

--- app.js
+++ app.js
@@ -9,16 +9,171 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Choice = require("inquirer/lib/objects/choice");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const teamMembersArray = [];
 
-inquirer.prompt([
+//Prompt questions 
+const questionsManager = [
     {
-        type: "input"   
+        type: "input",
+        message: "Enter manager's name? ",
+        name: "name",
+        // validate: function (value) {
+        //     if (value.length)  return true;
+        //      else  return 'Please enter your username or e-mail address.';
+    
+    },
+    {
+        type: "input",
+        message: "Enter manager id? ",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Enter manager's email? ",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Enter manager's office number? ",
+        name: "officeNumber"
     }
-])
+
+]
+const questionsEngineer = [
+    {
+        type: "input",
+        message: "Enter engineer name? ",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Enter engineer id? ",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Enter engineer email? ",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Enter engineer's github username? ",
+        name: "github"
+    }
+]
+const questionsIntern = [
+    {
+        type: "input",
+        message: "Enter intern name? ",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Enter intern id? ",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Enter intern email? ",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Enter intern school ",
+        name: "school"
+    }
+]
+
+//functions
+//function to ask to add more employees
+
+
+
+
+inquirer.prompt(questionsManager).then(answers => {
+
+    let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+    teamMembersArray.push(manager)
+    console.log(teamMembersArray);
+
+    const addMember = function () {
+        inquirer.prompt([
+            {
+                type: "confirm",
+                message: "Would you like to add member?",
+                name: "anotherMember"
+            }
+        ]).then(answers => {
+            if (answers.anotherMember) {
+                typeOfEmployee()
+
+            }
+
+        })
+
+    }
+
+  
+
+    addMember();
+    function typeOfEmployee() {
+     
+ 
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Add team member: ",
+                name: "role",
+                choices: ["Engineer", "Intern"]
+            
+
+            }
+        ]).then(answers => {
+            const role = answers.role
+
+
+         
+        switch (role) {
+                case "Engineer":
+                    inquirer.prompt(questionsEngineer)
+                        .then(answers => {
+                            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+                            teamMembersArray.push(engineer)
+                            addMember()
+
+                        
+                        });
+                    break;
+                case "Intern":
+                    inquirer.prompt(questionsIntern)
+                        .then(answers => {
+                            const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+                            teamMembersArray.push(intern)
+                            addMember()
+                        })
+                
+            }
+        
+        })
+
+        console.log("This is outside", teamMembersArray)
+
+
+    }
+
+
+    render(teamMembersArray)
+
+
+});
+
+
+
 
 
 // After the user has input all employees desired, call the `render` function (required
